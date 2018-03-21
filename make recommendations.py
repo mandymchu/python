27430eb693062ad5e4427ab37dad21bc73135867
Mandy C <35241868+yueerfish@users.noreply.github.com>
2018-03-21 19:50:32 -0400

    Add files via upload

---
+++ make recommendations.py
@@ -0,0 +1,5 @@
+import recommendations
+
+print recommendations.topMatches(recommendations.critics,'Toby',n=3)
+[(0.99124070716192991, 'Lisa Rose'), (0.92447345164190486, 'Mick LaSalle'),
+(0.89340514744156474, 'Claudia Puig')]
